9670ab6e52a68107cc2d2fb56a287962441663bf
Agtoutzi <grandthau@hotmail.com>
2017-03-02 13:20:37 +0200

    Imlement possession button function Add variable Add button functions Add option in display function

--- app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
@@ -11,6 +11,7 @@ import static com.example.android.footballstatscounter.R.id.score;
 public class MainActivity extends AppCompatActivity {
 
     int scoreA=0,scoreB=0;
+    int possession=50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +29,27 @@ public class MainActivity extends AppCompatActivity {
         display(1);
     }
 
+    public void gainPossessionA(View v){
+        if(possession<100)possession++;
+        display(2);
+    }
+
+    public void gainPossessionB(View v){
+        if(possession>0)possession--;
+        display(2);
+    }
+
     public void display(int code){
         switch (code){
             case 1:
                 TextView scoreView = (TextView) findViewById(R.id.score);
                 scoreView.setText(String.valueOf(scoreA) + " - " + String.valueOf(scoreB));
                 break;
-
+            case 2:
+                TextView possessionViewA = (TextView) findViewById(R.id.possessionA);
+                TextView possessionViewB = (TextView) findViewById(R.id.possessionB);
+                possessionViewA.setText(String.valueOf(possession) + "%");
+                possessionViewB.setText(String.valueOf(100 - possession) + "%");
         }
     }
 }
