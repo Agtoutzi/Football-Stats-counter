8d1fcde8a8942e51173749d0fcb7e395e198a888
Agtoutzi <grandthau@hotmail.com>
2017-03-02 12:54:40 +0200

    Implement goal buttons Add score variables Implement score goal functions

--- app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.android.footballstatscounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreA=0,scoreB=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goalTeamA(View v){
+        scoreA++;
+    }
+
+    public void goalTeamB(View v){
+        scoreB++;
     }
 }
