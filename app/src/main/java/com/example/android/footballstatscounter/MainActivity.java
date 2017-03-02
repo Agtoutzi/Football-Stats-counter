543e67f9462c175f252a55ab3f4c4bb9e0fab18a
Agtoutzi <grandthau@hotmail.com>
2017-03-02 13:35:47 +0200

    Implement shots buttons Add variables Add functions Add display options

--- app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballstatscounter/MainActivity.java
@@ -6,12 +6,14 @@ import android.view.View;
 import android.widget.TextView;
 
 import static android.R.attr.value;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 import static com.example.android.footballstatscounter.R.id.score;
 
 public class MainActivity extends AppCompatActivity {
 
     int scoreA=0,scoreB=0;
     int possession=50;
+    int shotsA=0,shotsB=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
         display(2);
     }
 
+    public void shotA(View v){
+        shotsA++;
+        display(11);
+    }
+
+    public void shotB(View v){
+        shotsB++;
+        display(21);
+    }
+
     public void display(int code){
         switch (code){
             case 1:
@@ -50,6 +62,16 @@ public class MainActivity extends AppCompatActivity {
                 TextView possessionViewB = (TextView) findViewById(R.id.possessionB);
                 possessionViewA.setText(String.valueOf(possession) + "%");
                 possessionViewB.setText(String.valueOf(100 - possession) + "%");
+                break;
+            case 11:
+                TextView shotViewA = (TextView) findViewById(R.id.shotsA);
+                shotViewA.setText(String.valueOf(shotsA));
+                break;
+            case 21:
+                TextView shotViewB = (TextView) findViewById(R.id.shotsB);
+                shotViewB.setText(String.valueOf(shotsB));
+                break;
+
         }
     }
 }
